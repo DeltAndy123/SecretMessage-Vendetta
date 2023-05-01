86d51e3373140992da061a6c4142be1f5d6636f9
DeltAndy123 <deltandy123@gmail.com>
2023-05-01 19:21:02 -0400

    Decrypt messages in edit and message load

--- plugins/SecretMessage/src/index.ts
+++ plugins/SecretMessage/src/index.ts
@@ -5,9 +5,18 @@ import { decryptMessage } from "./util/encrypt";
 
 const unload = [
   patcher.before("dispatch", metro.common.FluxDispatcher, ([e]) => {
-    if (e.type == "MESSAGE_CREATE") {
-      e.message.content = decryptMessage(e.message.content);
-      return [e];
+    switch (e.type) {
+      case "MESSAGE_CREATE":
+        e.message.content = decryptMessage(e.message.content);
+        return [e];
+      case "MESSAGE_UPDATE":
+        e.message.content = decryptMessage(e.message.content);
+        return [e];
+      case "LOAD_MESSAGES_SUCCESS":
+        e.messages.forEach((m) => {
+          m.content = decryptMessage(m.content);
+        });
+        return [e];
     }
   }),
 ];
