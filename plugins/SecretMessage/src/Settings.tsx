23f9db193fe35018d3743b049ec8c3ec42287314
DeltAndy123 <deltandy123@gmail.com>
2023-05-01 21:11:32 -0400

    Fix settings

--- plugins/SecretMessage/src/Settings.tsx
+++ plugins/SecretMessage/src/Settings.tsx
@@ -1,47 +1,41 @@
 import { React, ReactNative } from "@vendetta/metro/common";
 import { Forms, General } from "@vendetta/ui/components";
 import { storage } from "@vendetta/plugin";
+import { useProxy } from "@vendetta/storage";
 const { FormText, FormRow, FormInput, FormSwitch } = Forms;
 // const { TextInput } = General;
 const { ScrollView } = ReactNative;
 
-export default () => (
+export default () => {
+  useProxy(storage);
 
-<ScrollView>
-  {/* <FormRow
-    label={<TextInput
-      value={storage.key}
-      onSubmitEditing={(e) => storage.key = e.nativeEvent.text}
-      placeholder="Secret key to encrypt messages"
-      returnKeyType="done"
-      secureTextEntry={true}
-      defaultValue={storage.key || "default"}
-    />}
-  /> */}
-  <FormInput
-    title="Key"
-    value={storage.key || "default"}
-    onSubmitEditing={(e) => storage.key = e.nativeEvent.text}
-    placeholder="Secret key to encrypt messages"
-    returnKeyType="done"
-    secureTextEntry={true}
-  />
-  <FormRow
-    label="Auto shorten text"
-    subLabel="Shorten encrypted text by replacing specific char. Can be detected by AutoMod."
-    trailing={<FormSwitch
-      value={storage.shorten_text}
-      onValueChange={(value) => storage.shorten_text = value}
-    />}
-  />
-  <FormRow
-    label="Debug Mode"
-    subLabel="Log debug messages to console."
-    trailing={<FormSwitch
-      value={storage.debug}
-      onValueChange={(value) => storage.debug = value}
-    />}
-  />
-</ScrollView>
+  return (
+    <ScrollView>
+      <FormInput
+        title="Key"
+        value={storage.key ?? "default"}
+        onSubmitEditing={(e) => storage.key = e.nativeEvent.text}
+        placeholder="Secret key to encrypt messages"
+        returnKeyType="done"
+        secureTextEntry={true}
+      />
+      <FormRow
+        label="Auto shorten text"
+        subLabel="Shorten encrypted text by replacing specific char. Can be detected by AutoMod."
+        trailing={<FormSwitch
+          value={storage.shorten_text ?? true}
+          onValueChange={(value: boolean) => storage.shorten_text = value}
+        />}
+      />
+      <FormRow
+        label="Debug Mode"
+        subLabel="Log debug messages to console."
+        trailing={<FormSwitch
+          value={storage.debug ?? false}
+          onValueChange={(value: boolean) => storage.debug = value}
+        />}
+      />
+    </ScrollView>
+  )
 
-);
+};
