d3b2f1dc150145859c4a259ff8e1e224d2fff140
DeltAndy123 <deltandy123@gmail.com>
2023-05-01 23:31:15 -0400

    Add slash command

--- plugins/SecretMessage/src/index.ts
+++ plugins/SecretMessage/src/index.ts
@@ -2,9 +2,14 @@ import { logger, metro, patcher } from "@vendetta";
 import Settings from "./Settings";
 import { storage } from "@vendetta/plugin";
 import { decryptMessage, encryptMessage, getSuffixRegex } from "./util/encrypt";
-import { findByProps } from "@vendetta/metro";
+import { findByDisplayName, findByProps } from "@vendetta/metro";
+import { registerCommand } from "@vendetta/commands";
+import { toasts } from "@vendetta/ui";
 
 const Messages = findByProps("sendMessage", "receiveMessage");
+const Pressable = findByDisplayName("Pressable");
+
+let giftButtonID = null
 
 const unload = [
   patcher.before("dispatch", metro.common.FluxDispatcher, ([e]) => {
@@ -24,6 +29,7 @@ const unload = [
     }
     if (storage.debug) logger.info(e);
   }),
+
   // Encrypt sent messages
   patcher.before("sendMessage", Messages, ([,msg]) => {
     if (storage.enable_encryption) {
@@ -37,8 +43,43 @@ const unload = [
   }),
   patcher.before("startEditMessage", Messages, ([,,content]) => {
     // Remove suffix (<key**>) from message when editing
+    // TODO: Fix not replacing the message starting to edit
     content = content.replace(getSuffixRegex(storage.key), "");
   }),
+
+  // Slash commands
+  registerCommand({
+    name: "togglesecretmessage",
+    displayName: "togglesecretmessage",
+    description: "Toggle SecretMessage",
+    displayDescription: "Toggle SecretMessage",
+    options: [
+      {
+        name: "enable",
+        displayName: "enable",
+        description: "Enable SecretMessage (If not specified, it will be toggled)",
+        displayDescription: "Enable SecretMessage (If not specified, it will be toggled)",
+        type: ApplicationCommandOptionType.BOOLEAN,
+        required: false,
+      }
+    ],
+    applicationId: "",
+    inputType: ApplicationCommandInputType.BUILT_IN_TEXT,
+    type: ApplicationCommandType.CHAT,
+
+    execute: (args, ctx) => {
+      if (args[0].value) {
+        storage.enable_encryption = true;
+        toasts.showToast("Encrypting messages enabled");
+      } else if (args[0].value === false) {
+        storage.enable_encryption = false;
+        toasts.showToast("Encrypting messages disabled");
+      } else {
+        storage.enable_encryption = !storage.enable_encryption;
+        toasts.showToast(`Encrypting messages ${storage.enable_encryption ? "enabled" : "disabled"}`);
+      }
+    }
+  })
 ];
 
 export function onUnload() {
